7937a882c31a91994cb47c14104e8de5f76186ad
김민수 <mkms1104@naver.com>
2025-05-30 13:42:52 +0900

    refector: 조합 및 VO 적용

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
@@ -2,13 +2,27 @@ package cleancode.minesweeper.tobe.io;
 
 import java.util.Scanner;
 
+import cleancode.minesweeper.tobe.BoardIndexConverter;
+import cleancode.minesweeper.tobe.position.CellPosition;
+
 public class ConsoleInputHandler implements InputHandler {
 
     public static final Scanner SCANNER = new Scanner(System.in);
 
+    private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
+    
     @Override
     public String getUserInput() {
         return SCANNER.nextLine();
     }
 
+    @Override
+    public CellPosition getCellPositionFromUser() {
+        String userInput = SCANNER.nextLine();
+        
+        int rowIndex = boardIndexConverter.getSelectedRowIndex(userInput);
+        int colIndex = boardIndexConverter.getSelectedColIndex(userInput);
+       
+        return CellPosition.of(rowIndex, colIndex);
+    }
 }
---
+++ src/main/java/cleancode/minesweeper/tobe/position/RelativePosition.java
@@ -0,0 +1,64 @@
+package cleancode.minesweeper.tobe.position;
+
+import java.util.List;
+
+public class RelativePosition {
+
+    public static final List<RelativePosition> SURROUNDED_POSITIONS = List.of(
+            RelativePosition.of(-1, -1),
+            RelativePosition.of(-1, 0),
+            RelativePosition.of(-1, 1),
+            RelativePosition.of(0, -1),
+            RelativePosition.of(0, 1),
+            RelativePosition.of(1, -1),
+            RelativePosition.of(1, 0),
+            RelativePosition.of(1, 1)
+    );
+
+    private final int deltaRow;
+    private final int deltaCol;
+
+    private RelativePosition(int deltaRow, int deltaCol) {
+        this.deltaRow = deltaRow;
+        this.deltaCol = deltaCol;
+    }
+
+    public static RelativePosition of(int deltaRow, int deltaCol) {
+        return new RelativePosition(deltaRow, deltaCol);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + deltaRow;
+        result = prime * result + deltaCol;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RelativePosition other = (RelativePosition) obj;
+        if (deltaRow != other.deltaRow)
+            return false;
+        if (deltaCol != other.deltaCol)
+            return false;
+        return true;
+    }
+
+    public int getDeltaRow() {
+        return deltaRow;
+    }
+
+    public int getDeltaCol() {
+        return deltaCol;
+    }
+
+    
+}
